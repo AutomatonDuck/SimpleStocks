e05fc06b803784db558b1ae475bbe2c76f284728
automatonduck <Sunflower7!>
2021-04-10 18:13:53 -0400

    moved response code to ListFragment.java, created JSONArray object

--- app/src/main/java/com/example/simplestocks/ListFragment.java
+++ app/src/main/java/com/example/simplestocks/ListFragment.java
@@ -1,28 +1,73 @@
 package com.example.simplestocks;
 
 
+import android.app.Application;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class ListFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    /*    try {
-            JSONObject stonks = new JSONObject(getIntent().getStringExtra("Response"));
-        }catch (Exception e){
-            System.out.println("****JSON PARSING ERROR****");
-        }*/
+        String url = "https://api.polygon.io/v2/reference/tickers?sort=ticker&perpage=50&page=1&apiKey=p43H0UpbcDsGNvlzCI62sVnwoapw4su_";
+
+
+            RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                    url,
+                    response -> {
+
+
+                        //System.out.println(response);
+                        try {
+                            JSONObject jordan = new JSONObject(response);
+                            //System.out.println(jordan);
+                            JSONArray tickers = jordan.getJSONArray("tickers");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            queue.add(stringRequest);
+
 
 
         return inflater.inflate(R.layout.fragment_list,container,false);
+
     }
+
 
 }
--- app/src/main/java/com/example/simplestocks/MainActivity.java
+++ app/src/main/java/com/example/simplestocks/MainActivity.java
@@ -3,25 +3,15 @@ package com.example.simplestocks;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
-    String url = "https://api.polygon.io/v2/reference/tickers?sort=ticker&perpage=50&page=1&apiKey=p43H0UpbcDsGNvlzCI62sVnwoapw4su_";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +21,8 @@ public class MainActivity extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
-        getTicker(url);
 
     }
-
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener(){
 
@@ -47,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new HomeFragment();
                             break;
                         case R.id.nav_ticker:
+
                             selectedFragment = new ListFragment();
                             break;
                         case R.id.nav_news:
@@ -62,43 +51,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
-    public void getTicker(String url) {
-        RequestQueue queue = Volley.newRequestQueue(this);
 
-
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET,
-                url,
-                response -> {
-                    //textView.setText("Response is: " + response.substring(0, 489)); //response is only length 489
-
-                    //System.out.println(response);
-                  try {
-                       JSONObject jordan = new JSONObject(response);
-
-                       //temp = main.getString("temp");
-                    } catch (JSONException e) {
-                       e.printStackTrace();
-                   }
-
-                    //tempView.setText("Temprature: " + temp);
-
-
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "No Response", Toast.LENGTH_SHORT).show(); // change to toast
-
-            }
-        });
-        queue.add(stringRequest);
-
-
-    }
-
-    public void sendResponse(JSONObject response){
-        Intent intent = new Intent(this, ListFragment.class);
-        intent.putExtra("Response",response.toString());
-        startActivity(intent);
-    }
 }
