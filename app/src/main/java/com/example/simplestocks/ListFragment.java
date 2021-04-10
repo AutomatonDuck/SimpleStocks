b861ec7242916b5575c025816bdb1420f04ba11c
automatonduck <Sunflower7!>
2021-04-10 16:01:27 -0400

    added volly and verified response from API for List of stocks

--- app/src/main/java/com/example/simplestocks/ListFragment.java
+++ app/src/main/java/com/example/simplestocks/ListFragment.java
@@ -10,10 +10,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    /*    try {
+            JSONObject stonks = new JSONObject(getIntent().getStringExtra("Response"));
+        }catch (Exception e){
+            System.out.println("****JSON PARSING ERROR****");
+        }*/
+
+
         return inflater.inflate(R.layout.fragment_list,container,false);
     }
+
 }
--- app/src/main/java/com/example/simplestocks/MainActivity.java
+++ app/src/main/java/com/example/simplestocks/MainActivity.java
@@ -4,13 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class MainActivity extends AppCompatActivity {
+    String url = "https://api.polygon.io/v2/reference/tickers?sort=ticker&perpage=50&page=1&apiKey=p43H0UpbcDsGNvlzCI62sVnwoapw4su_";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
+        getTicker(url);
 
     }
 
@@ -49,4 +61,44 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             };
+
+    public void getTicker(String url) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                url,
+                response -> {
+                    //textView.setText("Response is: " + response.substring(0, 489)); //response is only length 489
+
+                    //System.out.println(response);
+                  try {
+                       JSONObject jordan = new JSONObject(response);
+
+                       //temp = main.getString("temp");
+                    } catch (JSONException e) {
+                       e.printStackTrace();
+                   }
+
+                    //tempView.setText("Temprature: " + temp);
+
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "No Response", Toast.LENGTH_SHORT).show(); // change to toast
+
+            }
+        });
+        queue.add(stringRequest);
+
+
+    }
+
+    public void sendResponse(JSONObject response){
+        Intent intent = new Intent(this, ListFragment.class);
+        intent.putExtra("Response",response.toString());
+        startActivity(intent);
+    }
 }
